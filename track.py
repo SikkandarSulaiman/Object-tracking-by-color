c825a55e68bfbeba2a75ad9cde8b41719cde0a3e
Sikkandar Sulaiman <sikkandarsulaiman@gmail.com>
2017-09-29 23:53:25 +0530

    UI module to find color threshold of target object

--- track.py
+++ track.py
@@ -4,7 +4,7 @@ import numpy as np
 def noCallBack(nothing):
     pass
 
-def findThreshold(img):
+def findThreshold(img, thresh=[[255,255,255], [0,0,0]], erosion=0, dilation=0):
     
     cv2.namedWindow('image')
 
@@ -15,37 +15,50 @@ def findThreshold(img):
     cv2.createTrackbar('Upper V', 'image', 0, 255, noCallBack)
     cv2.createTrackbar('Lower V', 'image', 0, 255, noCallBack)
 
-    cv2.setTrackbarPos('Lower H', 'image', 255)
-    cv2.setTrackbarPos('Lower S', 'image', 255)
-    cv2.setTrackbarPos('Lower V', 'image', 255)
+    cv2.createTrackbar('Erode', 'image', 0, 20, noCallBack)
+    cv2.createTrackbar('Dilate', 'image', 0, 20, noCallBack)
 
-    while(1):
-        
-        k = cv2.waitKey(1) & 0xFF
-        if k == 27 or k == 13:
-            break
+    cv2.setTrackbarPos('Upper H', 'image', thresh[1][0])
+    cv2.setTrackbarPos('Upper S', 'image', thresh[1][1])
+    cv2.setTrackbarPos('Upper V', 'image', thresh[1][2])
+    cv2.setTrackbarPos('Lower H', 'image', thresh[0][0])
+    cv2.setTrackbarPos('Lower S', 'image', thresh[0][1])
+    cv2.setTrackbarPos('Lower V', 'image', thresh[0][2])
 
-        ru = cv2.getTrackbarPos('Upper H','image')
-        gu = cv2.getTrackbarPos('Upper S','image')
-        bu = cv2.getTrackbarPos('Upper V','image')
-        rl = cv2.getTrackbarPos('Lower H','image')
-        gl = cv2.getTrackbarPos('Lower S','image')
-        bl = cv2.getTrackbarPos('Lower V','image')
+    cv2.setTrackbarPos('Erode', 'image', erosion)
+    cv2.setTrackbarPos('Dilate', 'image', dilation)
 
-        obj_u = np.array([ru, gu, bu])
-        obj_l = np.array([rl, gl, bl])
+    while True:
+
+        upperH = cv2.getTrackbarPos('Upper H','image')
+        upperS = cv2.getTrackbarPos('Upper S','image')
+        upperV = cv2.getTrackbarPos('Upper V','image')
+        lowerH = cv2.getTrackbarPos('Lower H','image')
+        lowerS = cv2.getTrackbarPos('Lower S','image')
+        lowerV = cv2.getTrackbarPos('Lower V','image')
+        erosion = cv2.getTrackbarPos('Erode', 'image')
+        dilation = cv2.getTrackbarPos('Dilate', 'image')
+
+        upperThresh = np.array([upperH, upperS, upperV])
+        lowerThresh = np.array([lowerH, lowerS, lowerV])
 
         _, frame = img.read()
         hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        filtered = cv2.inRange(hsvFrame, obj_u, obj_l)
+        hsvFiltered = cv2.inRange(hsvFrame, upperThresh, lowerThresh)
+        eroded = cv2.erode(hsvFiltered, None, iterations=erosion)
+        dilated = cv2.dilate(eroded, None, iterations=dilation)
 
-        cv2.putText(filtered, 'Filter the object and press Enter to fix thresholds',
+        cv2.putText(dilated, 'Filter the object and press Enter to fix thresholds',
                     (0,20), cv2.FONT_HERSHEY_PLAIN, 1.3, (255,0,0), 1)
         
-        cv2.imshow('image', filtered)
+        cv2.imshow('image', dilated)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27 or key == 13:
+            break
 
     cv2.destroyAllWindows()
-    return [obj_u, obj_l]
+    return [[upperThresh, lowerThresh],[erosion, dilation]]
 
 if __name__ == '__main__':
     img = cv2.VideoCapture(0)
