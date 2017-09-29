d9c3465d8ea6623faec72b942e3e2f5c4d0335d7
Sikkandar Sulaiman <sikkandarsulaiman@gmail.com>
2017-09-29 22:42:47 +0530

    File to run

--- draw.py
+++ draw.py
@@ -1,20 +1,25 @@
 import cv2
-import numpy as np
 from que import Queue
+from track import findThreshold
+
 Q=Queue()
 
-obj_l = np.array([0,0,0])
-obj_u = np.array([255,255,255])
-img=cv2.VideoCapture(0)
+img = cv2.VideoCapture(0)
+
+[lowerThresh, upperThresh], [erosion, dilation] = findThreshold(img)
+
 while True:
     _, frame = img.read()
-    h,w,_=frame.shape
+    h, w, _ = frame.shape
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    obj = cv2.inRange(hsv, obj_l, obj_u)
-    obj = cv2.erode(obj, None, iterations=1)
-    obj = cv2.dilate(obj, None, iterations=1)
-    cv2.imshow("debug",obj)
-    c_obj,_ = cv2.findContours(obj, 1, 2)
+    obj = cv2.inRange(hsv, lowerThresh, upperThresh)
+    obj = cv2.erode(obj, None, iterations=erosion)
+    obj = cv2.dilate(obj, None, iterations=dilation)
+    cv2.putText(bbj, 'Press enter to tune threshold values',
+                    (0,20), cv2.FONT_HERSHEY_PLAIN, 1.3, (255,0,0), 1)
+    cv2.imshow("debug", obj)
+    c_obj, _ = cv2.findContours(obj, 1, 2)
+
     for c in c_obj:
         M = cv2.moments(c)
         if M["m00"] == 0:
@@ -22,19 +27,29 @@ while True:
         dX = int(M["m10"] / M["m00"])
         dY = int(M["m01"] / M["m00"])
         cv2.drawContours(frame, c_obj, -1, (0,255,255), 3)
-        (x,y),r = cv2.minEnclosingCircle(c)
-        cv2.circle(frame, (int(x),int(y)), int(r), (0,0,255), 3)
-        Q.enq((dX,dY))
+        (x, y), r = cv2.minEnclosingCircle(c)
+        cv2.circle(frame, (int(x), int(y)), int(r), (0,0,255), 3)
+        Q.enq((dX, dY))
         Q.deq
-        for i in range(0,20):
+        for i in xrange(0,20):
             try:
-                cv2.line(frame,Q.ret(i),Q.ret(i+1),(255,0,0),3)
+                cv2.line(frame, Q.ret(i), Q.ret(i+1), (255,0,0), 3)
             except IndexError:
                 pass
         cv2.putText(frame, str(int(x))+" "+str(int(y)),
-                    (dX-10,dY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (55,120,255),2)
+                    (dX-10,dY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (55,120,255), 2)
+    
     cv2.imshow("cont",frame)
-    if cv2.waitKey(1) == 27:
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
         break
+    elif key == 13:
+        cv2.destroyAllWindows()
+        findThreshold(img)
+
 img.release()
 cv2.destroyAllWindows()
+
+
+
